20046a4dedb499d735a8156d0bbefcc7c019df20
elie195 <elie195@gmail.com>
2017-01-02 11:21:41 +0200

    Misc cleanup

--- nginx/rewrites.lua
+++ nginx/rewrites.lua
@@ -1,21 +1,17 @@
   local redis = require "resty.redis"
   local red = redis:new()
-  -- local dbip = DBIP 
-  -- ngx.log(ngx.ERR, "heres the IP: ", dbip)
   -- see all available redis: methods @ https://github.com/openresty/lua-resty-redis#methods
   local ok, err = red:connect("db", 6379)
   if not ok then
     ngx.log(ngx.ERR, "failed to connect to redis: ", err)
     return ngx.exit(500)
   end
-  -- red:select(1)
   red:set_timeout(1000)
+  -- used for testing to popuate DB
   red:set("/test", "/testing")
   red:set("/DOC-1234", "/IT/Awesome")
   red:set("/DOC-2351", "/IT/Rules")
-  -- red:set_keepalive(0, 100)
-  -- ngx.log(ngx.ERR, "URI: ", ngx.var.request_uri)
   local redirect_candidate = ngx.var.request_uri
-  -- ngx.say(redirect_candidate)
   local rewrite_value, err = red:get(redirect_candidate)
+  -- modify the next line to suit your needs
   ngx.redirect("http://ubuntu" .. rewrite_value, 301);
--- nginx/start.sh
+++ nginx/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#/bin/sed -i "s/DBIP/${DB_PORT_6379_TCP_ADDR}/" /etc/nginx/rewrites/new_rewrites.lua
-#cat /etc/nginx/rewrites/new_rewrites.lua
 echo "IGNORE_RESOLVCONF=yes" >> /etc/default/dnsmasq
 echo "DNSMASQ_OPTS=\"-q\"" >> /etc/default/dnsmasq
 service dnsmasq restart
