8f1fdf25adba7770dc391e291f1f3ff54362aad5
elie195 <elie195@gmail.com>
2017-01-02 17:42:56 +0200

    Added health check at /pong

--- nginx/rewrites.lua
+++ nginx/rewrites.lua
@@ -8,13 +8,15 @@ if not ok then
 end
 red:set_timeout(1000)
 -- used for testing to popuate DB
-red:set("/test", "/testing")
+red:set("/ping", "pong")
 red:set("/DOC-1234", "/IT/Awesome")
 red:set("/DOC-2351", "/IT/Rules")
 local redirect_candidate = ngx.var.request_uri
 local rewrite_value, err = red:get(redirect_candidate)
 -- modify the next line to suit your needs
-if rewrite_value ~= ngx.null then
+if rewrite_value == "pong" then
+  ngx.say("PONG")
+elseif rewrite_value ~= ngx.null then
   ngx.redirect("http://ubuntu" .. rewrite_value, 301);
 else
   ngx.exit(400)
