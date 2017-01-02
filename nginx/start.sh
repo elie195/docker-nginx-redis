b866678f58dfe63f7342fd01c1b3b6c1017eb1cd
elie195 <elie195@gmail.com>
2017-01-02 11:52:47 +0200

    Populate db from redis.txt

--- nginx/start.sh
+++ nginx/start.sh
@@ -3,4 +3,8 @@
 echo "IGNORE_RESOLVCONF=yes" >> /etc/default/dnsmasq
 echo "DNSMASQ_OPTS=\"-q\"" >> /etc/default/dnsmasq
 service dnsmasq restart
+
+#Populate redis
+(cat /tmp/redis.txt; sleep 5) | nc db 6379 > /dev/null
+
 /usr/local/openresty/bin/openresty -g 'daemon off;'
